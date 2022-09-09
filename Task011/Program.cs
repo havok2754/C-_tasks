e362e76471bf6e415186eb0d02fcbec32c6f3fbf
havok <po4ta@example.com>
2022-09-09 22:52:36 +0700

    seminar1

---
+++ Task011/Program.cs
@@ -0,0 +1,16 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100, 1000);
+
+int Number2(int num)
+{
+int newnumber = (num / 100) * 10 + num % 10;
+return newnumber;
+}
+int result = Number2(number);
+Console.WriteLine($"{number} -> {result}");
---
+++ Task09/Program.cs
@@ -0,0 +1,31 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+// 88 -> цифры одинаковые
+
+int number = new Random().Next(10, 100);
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if(firstDigit > secondDigit) Console.WriteLine($"наибольщая цифра числа {number} - {firstDigit}");
+// else Console.WriteLine($"наибольщая цифра числа {number} - {secondDigit}");
+// if (firstDigit == secondDigit) Console.WriteLine($" в числе {number} цифры одинаковые");
+// else 
+// {
+// int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"наибольщая цифра числа {number} - {max}");
+// }
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+string result = maxDigit > 0 ? maxDigit.ToString() :  "цифры одинаковые";
+Console.WriteLine($"наибольщая цифра числа {number} - {result}");
